fae1ff9d9737c04c6cdaa5b143d5fb73d90d1d13
Andrew Bariu <andrew.bariu@student.moringaschool.com>
2025-05-24 12:08:41 +0300

    done owner

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -11,4 +11,16 @@ class Pet:
         Pet.all.append(self)
 
 class Owner:
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise Exception("Can only add instances of Pet")
+        pet.owner = self
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key=lambda pet: pet.name)
